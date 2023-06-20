79663039033c916b0e071559858bbaf5587d1488
Claudio Fuentes <claudiofuentesperez@gmail.com>
2023-06-19 23:40:33 -0400

    ya todos integrados los menues y aparecen y desaparecen de buena forma

--- js/main.js
+++ js/main.js
@@ -11,14 +11,35 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleDescktopMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
   
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+        aside.classList.add ('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
-}
+} 
+
 
 function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains ('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains ('inactive');
+
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add ('inactive');
+    }
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add ('inactive');
+    }
     aside.classList.toggle('inactive');
 }
